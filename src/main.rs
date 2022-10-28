9b81f7df532608de98d81033aed1919e1fe4255a
ymatienzo <yulemata@gmail.com>
2022-10-29 00:50:15 +0200

    start

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+}
+
+struct Server {
+    addr: String,
+}
+
+impl Server {
+
+    fn new(addr: String) -> Self {
+        Self { addr }
+    }
+
+    fn run(self){
+        print!("listening on {}",self.addr);
+
+    }
+}
